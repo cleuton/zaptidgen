a1e1b99ae080e93775dd6c2bcc16318563972163
cleutonsampaio <cleutonsampaio@cloud.upwork.com>
2023-09-20 17:32:49 -0300

    initial version

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/cleuton/zaptidgen
+
+go 1.19
+
+require (
+	github.com/sony/sonyflake v1.2.0
+	google.golang.org/grpc v1.58.1
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+)
---
+++ zaptidgen.go
@@ -0,0 +1,69 @@
+// Copyright 2023 Cleuton Sampaio.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/cleuton/zaptidgen/gen"
+
+	"github.com/sony/sonyflake"
+	"google.golang.org/grpc"
+)
+
+// ***** Global vars *****
+const VERSION = "0.1.0"
+
+var logger log.Logger = *log.Default()
+var st sonyflake.Settings
+var flake = sonyflake.NewSonyflake(sonyflake.Settings{})
+
+// ***** Protobuf server *****
+type server struct {
+	pb.UnimplementedIdGenServer
+}
+
+func (s *server) Gen(ctx context.Context, input *pb.IdRequest) (*pb.IdResponse, error) {
+	errorCode := false
+	sequence := uint64(0)
+	if flake == nil {
+		logger.Panicf("FATAL ERROR: Couldn't generate sonyflake.NewSonyflake.\n")
+	}
+	sequence, err := flake.NextID()
+	if err != nil {
+		errorCode = true
+		log.Printf("ERROR: flake.NextID() failed with %s\n", err)
+	}
+	return &pb.IdResponse{Error: errorCode, Id: sequence}, nil
+}
+
+// This is zapt.chat ID generator server.
+// A gRPC server that exports unique sequence of uint64 numbers.
+// It uses the last 16 bits of machine's IP address as "machine number".
+func main() {
+	logger.Printf("INFO: ZAPTIDGEN - V %s\n - starting... (port: 8888)", VERSION)
+
+	lis, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		logger.Panicf("FATAL ERROR: failed to listen: %v\n", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterIdGenServer(s, &server{})
+	logger.Printf("INFO: server listening at %v\n", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		logger.Panicf("FATAL ERROR: failed to serve: %v\n", err)
+	}
+}
